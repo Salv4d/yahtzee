693b3df4390240c44ce2b739d21f7f9dd8226035
Salvador Oliveira <slsalvadorlucas@gmail.com>
2022-02-21 22:14:42 -0400

    fix die locking

--- src/Dice.js
+++ src/Dice.js
@@ -1,18 +1,31 @@
-import React, { Component } from 'react';
-import Die from './Die';
-import './Dice.css';
+import React, { Component } from "react";
+import Die from "./Die";
+import "./Dice.css";
 
 class Dice extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(idx) {
+    this.props.toggleLocked(idx);
+  }
+
   render() {
-    return <div className="Dice">
-      {this.props.dice.map((d, idx) =>
-        <Die handleClick={this.props.handleClick}
-          val={d}
-          locked={this.props.locked[idx]}
-          idx={idx}
-          key={idx} />
-      )}
-    </div>
+    return (
+      <div className="Dice">
+        {this.props.dice.map((d, idx) => (
+          <Die
+            toggleLocked={this.handleClick}
+            val={d}
+            locked={this.props.locked[idx]}
+            idx={idx}
+            key={idx}
+          />
+        ))}
+      </div>
+    );
   }
 }
 
